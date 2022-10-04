7ff99f42e3a262cdf28859cbc9260bef727926e1
wilvo970 <wilvo970@student.liu.se>
2022-10-04 11:53:24 +0200

    something wrong with merge sort

--- merge/merge.cpp
+++ merge/merge.cpp
@@ -1,13 +1,95 @@
 #include <vector>
 #include <string>
+#include <iterator>
 
 #include "../help_functions.h"
 
 using namespace std;
+using Size_Type = long int;
+using Iterator_Type  = vector<string>::iterator;
+
+vector<string> merge_sort(Iterator_Type begin, Iterator_Type last)
+{
+    vector<string> result {};
+    vector<string> left {};
+    vector<string> right {};
+    Size_Type size{ distance(begin, last) + 1 };
+
+    /* partition array */
+    if (size == 1)
+    {
+        result.push_back(*begin);
+        return result;
+    }
+    else if(size == 2)
+    {
+        // Left side
+        left = merge_sort(begin, begin);
+    
+        // Right side    
+        right = merge_sort(last, last);
+    }
+    else
+    {
+        Iterator_Type middle { last - size/2};
+        // Left side
+        left = merge_sort(begin, middle);
+        
+        // Right side    
+        right = merge_sort(middle + 1, last);    
+    }
+
+    /* Sort returned partitions */
+    auto left_it {left.begin()};
+    auto right_it {right.begin()};
+    
+    auto right_end {right.end()};
+    auto left_end {left.end()};
+    
+    while (true)
+    {
+        if ( left_it->size() < right_it->size())
+        {
+            result.push_back(*left_it);
+            ++left_it;
+        }
+        if ( right_it->size() < left_it->size())
+        {
+            result.push_back(*right_it);
+            ++right_it;
+        }
+        else
+        {
+            result.push_back(*left_it);     // left_it and right_it point to a words the same size, thus choose one to push_back into vector.
+            ++left_it;
+        }
+
+        if (right_it == right_end)
+        {
+            for (; left_it != left_end; ++left_it)
+            {
+                result.push_back(*left_it);
+            }
+            break;
+        }
+        else if ( left_it == left_end )
+        {
+            for (; right_it != right_end; ++right_it)
+            {
+                result.push_back(*right_it);
+            }
+            break;
+        }
+    }
+
+}
+
+//cout << "size: " << size << ": " << *begin << " " << *last << endl;
+//copy(begin, middle + 1, ostream_iterator<string>(cout, " "));   // middle räknas som t.ex. vector.end(), alltså inkluderas 
 
 void sort(vector<string> & words)
 {
-
+    words = merge_sort(words.begin(), words.end() - 1);
 }
 
 int main()
@@ -17,7 +99,7 @@ int main()
     print(words);
 
     sort(words);
-    print_nice(words);
+    print(words);
 
     return 0;
 }
